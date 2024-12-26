02294cf9981966feee6c433dd7c7a1888c89334c
agnihotri-anxh <agnihotriansh05@gmail.com>
2024-12-26 16:44:38 +0530

    Hyperpamerter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,11 +16,9 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-
-# model_trainer.py
-
 from src.utils import save_object, evaluate_models
 
+@dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
@@ -38,17 +36,46 @@ class ModelTrainer:
                 test_array[:, -1],
             )
             models = {
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(criterion='mse'),
+                "Decision Tree": DecisionTreeRegressor(criterion='mse'),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            params = {
+                "Decision Tree": {
+                    'criterion': ['mse', 'friedman_mse', 'mae', 'poisson'],
+                },
+                "Random Forest": {
+                    'criterion': ['mse', 'mae', 'poisson'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'criterion': ['mse', 'friedman_mse'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
 
             model_report: dict = evaluate_models(
-                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models
+                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params
             )
             
             best_model_score = max(sorted(model_report.values()))
